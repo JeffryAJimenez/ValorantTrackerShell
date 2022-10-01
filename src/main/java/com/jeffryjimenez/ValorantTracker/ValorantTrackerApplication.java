533cca454cc99781a2d51b637eff6e22d8cb459f
JeffryAJimenez <jeffry.jimenez@outlook.com>
2022-10-01 13:29:08 -0400

    first commmit

---
+++ src/main/java/com/jeffryjimenez/ValorantTracker/ValorantTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jeffryjimenez.ValorantTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValorantTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffryjimenez/ValorantTracker/ValorantTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffryjimenez.ValorantTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValorantTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
